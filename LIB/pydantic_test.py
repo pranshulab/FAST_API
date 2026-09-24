dd509c848b45e0b56d13a014b0880e31a241c4a0
Pranshu katiyar <pranshukatiyar1511@gmail.com>
2026-09-25 00:15:30 +0530

    going throw Annotated and some constraint

--- LIB/pydantic_test.py
+++ LIB/pydantic_test.py
@@ -1,18 +1,18 @@
 
-from pydantic import BaseModel, EmailStr, AnyUrl
-from typing import Dict, Optional
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import Dict, Optional, Annotated
 from typing import List
 
 
 class Patient(BaseModel):
     
-    name: str
-    age : int
-    # email: EmailStr
+    name: Annotated[str, Field(max_length = 50, title = 'name of the patient', description = 'give the name of the patient in less than 50 chars', examples =['aman','Harsh'])]
+    age : int = Field(gt=0, lt=120)
+    email: EmailStr
     linkedin_url : AnyUrl
-    weight: float
-    married: bool
-    allergies: Optional[list[str]] = None
+    weight: Annotated[float, Field(gt=0, strict = True, description = 'weight of the patient in kg')]
+    married: Annotated[bool, Field(default=None, description='is the patient married or not')]
+    allergies: Annotated[Optional[list[str]], Field(default = None,max_length = 5)]
     contact_details: Dict[str,str]
 
 def insert_patient_data(patient: Patient):
@@ -27,7 +27,7 @@ def update_patient_data(patient: Patient):
     print(patient.age)
     print('updated')
             
-patient_info = {'name': 'Pranshu', 'age': 23, 'linkedin_url' : 'https://linkedin/001', 'weight': 68.7, 'married': True,
+patient_info = {'name': 'Pranshu', 'age': 23, 'email': 'abc@gmail.com', 'linkedin_url' : 'https://linkedin/001', 'weight': 68.7, 'married': True,
                 'contact_details' :{'phone': '123456789'}}
 
 patient1 = Patient(**patient_info)
